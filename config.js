baa37763c905c5eae1f4edd1321d11618ebeabff
Aslyxiasys <einsaslyxia@gmail.com>
2025-12-02 23:55:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "fall-2025-openings",
-    title: "Fall 2025 Openings Party Rank Sorter",
-    description: "Party rank sorter for Fall 2025 Openings."
+    localStoragePrefix: "test-run-ygo",
+    title: "Yu-Gi-Oh! Opening Ranking",
+    description: "Yu-Gi-Oh! Opening Ranking"
 };
